f1497698cec770d09f1993371b2d9facb63560e4
Marco Galardini <marco@ebi.ac.uk>
2015-10-20 12:07:14 +0100

    Fix the startup script

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-$(REPODIR)ecoliNTdb/src/get_strains_genomes --db $(REPODIR)ecoliNTdb/ntdb.sqlite | src/get_genomes_id > genomes.start.txt
+ecoliNTdb/src/get_strains_genomes --db ecoliNTdb/ntdb.sqlite | src/get_genomes_id > genomes.start.txt
 mkdir -p genomes
 mkdir -p proteomes
 for genome in $(awk '{print $1}' genomes.start.txt)
 do
     echo $genome;
-    src/gbk2fasta $(REPODIR)ecoliNTdb/genomes/$(grep $genome genomes.start.txt | awk '{print $2}') genomes/$genome.fasta;
-    src/gbk2faa $(REPODIR)ecoliNTdb/genomes/$(grep $genome genomes.start.txt | awk '{print $2}') proteomes/$genome.faa;
+    src/gbk2fasta ecoliNTdb/genomes/$(grep $genome genomes.start.txt | awk '{print $2}') genomes/$genome.fasta;
+    src/gbk2faa ecoliNTdb/genomes/$(grep $genome genomes.start.txt | awk '{print $2}') proteomes/$genome.faa;
 done
