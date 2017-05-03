e3002b0885c0ef41001a7022d85e245c5a6588f4
Marco Galardini <marco@ebi.ac.uk>
2017-05-03 17:10:18 +0100

    Change VCF merging routine

--- merge_vcf.sh
+++ merge_vcf.sh
@@ -2,13 +2,13 @@
 
 mkdir all_vcf
 find pout -maxdepth 1 -type f -name '*.vcf' ! -name '*.merged.*' ! -name '*.nonsyn.*' ! -name '*.stop.*' ! -name '*.annotated.*' -exec cp {} all_vcf/ \;
+for i in $(cat outgroups.txt); do rm all_vcf/$i*; done
 cd all_vcf
 for i in $(find . -name '*.vcf'); do bgzip $i && tabix -p vcf $i.gz; done
 vcf-merge *.vcf.gz > all.vcf
 rm keep.txt
 for i in $(find . -type f -name '*.vcf.gz'); do echo $(echo $i | sed 's/.\///g' | sed 's/.vcf.gz//g').fasta >> keep.txt; done
-cat all.vcf | vcf-subset -r -e -c keep.txt -f > subset.vcf
-../src/vcf_filter subset.vcf | grep -v "##source" | grep -v "##INFO" > snps.vcf
+cat all.vcf | vcf-subset -e -c keep.txt -f | ../src/vcf_filter | grep -v "##source" | grep -v "##INFO" > snps.vcf
 bgzip snps.vcf
 rm subset.vcf
 rm all.vcf
